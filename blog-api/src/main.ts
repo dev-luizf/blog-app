a9521135c2327fcee3246d97cb367f6179c8715c
dev-luizf <luizfurtadomkt@hotmail.com>
2023-07-03 16:35:29 -0300

    feat: configura o prisma e conexão com o banco

--- blog-api/src/main.ts
+++ blog-api/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 
   const config = new DocumentBuilder()
     .setTitle('Blog API')
