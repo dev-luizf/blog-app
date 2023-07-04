15298d3df52d208bbc5df2ebd238cf1d75aa435a
dev-luizf <luizfurtadomkt@hotmail.com>
2023-07-04 17:57:57 -0300

    feat: implementa página de login e registro

--- blog-ui/src/App.tsx
+++ blog-ui/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './global.css';
+import Auth from './pages/Auth';
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Auth />} />
           {/* <Route path="/admin" element={<Admin />} /> */}
-          {/* <Route path="/login" element={<Login />} /> */}
           {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </BrowserRouter>
