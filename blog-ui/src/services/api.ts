3b492d0543a7336d97ab33670cd8769da5622f5e
dev-luizf <luizfurtadomkt@hotmail.com>
2023-07-05 02:43:20 -0300

    feat: implementa interceptor de token

--- blog-ui/src/services/api.ts
+++ blog-ui/src/services/api.ts
@@ -7,4 +7,12 @@ const api = axios.create({
   baseURL: 'http://localhost:3001',
 });
 
+api.interceptors.request.use((request) => {
+  const access_token = localStorage.getItem('token');
+  if (access_token && request.headers) {
+    request.headers.Authorization = `Bearer ${access_token}`;
+  }
+  return request;
+});
+
 export default api;
