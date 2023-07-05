3b3668de81fd7a5c1966f66f4fe3e536a6c74489
dev-luizf <luizfurtadomkt@hotmail.com>
2023-07-04 21:13:10 -0300

    refactor: adiciona acess token no registro

--- blog-api/src/users/users.module.ts
+++ blog-api/src/users/users.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/auth/constants';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
--- blog-api/src/users/users.service.ts
+++ blog-api/src/users/users.service.ts
@@ -7,10 +7,11 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import argon2Helper from 'src/utils/argon2Helper';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
 
   async create(createUserDto: CreateUserDto) {
     const usernameExists = await this.findByUsername(createUserDto.username);
@@ -24,7 +25,11 @@ export class UsersService {
       password: passwordHash,
     };
 
-    await this.prisma.user.create({ data: newUser });
+    const user = await this.prisma.user.create({ data: newUser });
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 
   findAll() {
