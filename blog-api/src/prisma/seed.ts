9e4cf84a0e222760e14820c40c141ebc826901c7
dev-luizf <luizfurtadomkt@hotmail.com>
2023-07-05 05:56:52 -0300

    refactor: atualiza seed com usuários e remove ids

--- blog-api/src/prisma/seed.ts
+++ blog-api/src/prisma/seed.ts
@@ -1,8 +1,20 @@
 import { PrismaService } from './prisma.service';
 
+const authors = [
+  {
+    username: 'luiz',
+    password: "$argon2id$v=19$m=65536,t=3,p=4$ewWDLrhTYRzT1+2uVwxy6Q$qSsecWHY52OXRZWV8Fn0iD+Nh1H2p5RdXVGIuK1ZY/8",
+    isAdmin: true,
+  },
+  {
+    username: 'teste',
+    password: "$argon2id$v=19$m=65536,t=3,p=4$ewWDLrhTYRzT1+2uVwxy6Q$qSsecWHY52OXRZWV8Fn0iD+Nh1H2p5RdXVGIuK1ZY/8",
+    isAdmin: true,
+  }
+];
+
 const posts = [
   {
-    id: 1,
     title: 'Introduction to Machine Learning',
     content:
       'Machine learning is a subset of artificial intelligence that focuses on the development of algorithms...',
@@ -12,7 +24,6 @@ const posts = [
     authorId: 1,
   },
   {
-    id: 2,
     title: 'The Benefits of Regular Exercise',
     content:
       'Regular exercise has numerous benefits for both physical and mental health...',
@@ -22,7 +33,6 @@ const posts = [
     authorId: 1,
   },
   {
-    id: 3,
     title: 'Exploring the Wonders of Space',
     content: 'Space has always been a subject of fascination for humans...',
     published: true,
@@ -31,30 +41,32 @@ const posts = [
     authorId: 1,
   },
   {
-    id: 4,
     title: 'Delicious and Healthy Recipes for Summer',
     content:
       'Summer is the perfect time to try out new recipes that are both delicious and nutritious...',
     published: true,
     createdAt: '2023-07-02T12:20:45Z',
     updatedAt: '2023-07-02T14:30:55Z',
-    authorId: 1,
+    authorId: 2,
   },
   {
-    id: 5,
     title: 'Tips for Improving Productivity',
     content:
       'Boosting productivity can have a significant impact on both personal and professional life...',
     published: true,
     createdAt: '2023-07-01T08:00:00Z',
     updatedAt: '2023-07-01T10:10:05Z',
-    authorId: 1,
+    authorId: 2,
   },
 ];
 
 const prisma = new PrismaService();
 
 async function main() {
+  await prisma.user.createMany({
+    data: authors,
+    skipDuplicates: true,
+  });
   await prisma.post.createMany({
     data: posts,
   });
